093bf3f424abf3d2e97cc6d01de43380fe816eb9
Paltipa <tag-arman@yandex.ru>
2015-07-27 20:03:22 +0300

    f

---
+++ WelcomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  WelcomeViewController.h
+//  HappyBirthday
+//
+//  Created by Арман Тагбергенев on 27.07.15.
+//  Copyright (c) 2015 Tagbergenev Arman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WelcomeViewController : UIViewController
+
+@end
