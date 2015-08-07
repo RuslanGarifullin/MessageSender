e235e55fc853c18acbf928798fab53dc1fb3ed4a
RuslanGarifullin <garifullinruslan11@gmail.com>
2015-08-07 03:50:20 +0300

    -add PatternViewController

---
+++ HappyBirthday/PatternViewController.h
@@ -0,0 +1,13 @@
+//
+//  PatternViewController.h
+//  HappyBirthday
+//
+//  Created by Ruslan on 07.08.15.
+//  Copyright (c) 2015 Tagbergenev Arman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PatternViewController : UIViewController
+
+@end
---
+++ HappyBirthday/PatternViewController.m
@@ -0,0 +1,37 @@
+//
+//  PatternViewController.m
+//  HappyBirthday
+//
+//  Created by Ruslan on 07.08.15.
+//  Copyright (c) 2015 Tagbergenev Arman. All rights reserved.
+//
+
+#import "PatternViewController.h"
+
+@interface PatternViewController ()
+
+@end
+
+@implementation PatternViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
